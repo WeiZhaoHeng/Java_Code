c17be5d11a2a3d26c3bb55352dadca2858199441
WeiZhaoHeng <1332113981@qq.com>
2019-11-26 17:40:18 +0800

    顺序表

---
+++ 2019_11_26/src/classCode.java
@@ -0,0 +1,24 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User：WeiZhaoheng
+ * Date: 2019/11/26
+ * Time: 15:41
+ */
+public class classCode {
+    public static void main(String[] args) {
+
+
+    MyArrayList myArrayList = new MyArrayList();
+        myArrayList.add(0,12);
+        myArrayList.add(0,13);
+        myArrayList.add(0,14);
+        myArrayList.add(0,15);
+        myArrayList.add(0,16);
+        myArrayList.add(0,17);
+        myArrayList.add(0,18);
+        myArrayList.display();
+        myArrayList.clear();
+        myArrayList.display();
+    }
+}
