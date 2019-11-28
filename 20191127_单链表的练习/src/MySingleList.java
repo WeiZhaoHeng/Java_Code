57eeb84314be89e60c02cfb2f2c8b8e26737d29c
WeiZhaoHeng <1332113981@qq.com>
2019-11-28 15:56:47 +0800

    单链表的完善

--- 20191127_单链表的练习/src/MySingleList.java
+++ 20191127_单链表的练习/src/MySingleList.java
@@ -7,7 +7,7 @@
  */
 class ListNode{
     public int data;
-    public List_Node next;
+    public ListNode next;
     public ListNode(int data){
         this.data = data;
         this.next = null;
@@ -15,14 +15,14 @@ class ListNode{
 }
 class MySingleList {
 
-    public List_Node head;
+    public ListNode head;
     public MySingleList(){
         this.head = null;
     }
 
     //头插法
     public void addFirst(int data){
-        List_Node node = new List_Node(data);
+        ListNode node = new ListNode(data);
         if(this.head == null){
             this.head = node;
         }else {
@@ -33,8 +33,8 @@ class MySingleList {
 
     //尾插法
     public void addLast(int data){
-        List_Node node = new List_Node(data);
-        List_Node cur = head.next;
+        ListNode node = new ListNode(data);
+        ListNode cur = head.next;
         if(this.head == null){
             this.head = node;
         }else{
@@ -50,7 +50,7 @@ class MySingleList {
         if(this.head == null){
             return;
         }
-        List_Node cur = this.head;
+        ListNode cur = this.head;
         while(cur != null){
             System.out.print(cur.data+" ");
             cur = cur.next;
@@ -63,8 +63,8 @@ class MySingleList {
      * @param index
      * @return
      */
-    private List_Node searchIndex(int index){
-        List_Node prev = this.head;
+    private ListNode searchIndex(int index){
+        ListNode prev = this.head;
         int count =0;
         while(count < index-1){
             prev = prev.next;
@@ -81,8 +81,8 @@ class MySingleList {
             addLast(data);
             return true;
         }
-        List_Node prev = searchIndex(index);
-        List_Node node = new List_Node(data);
+        ListNode prev = searchIndex(index);
+        ListNode node = new ListNode(data);
         node.next = prev.next;
         prev.next = node ;
         return true;
@@ -90,12 +90,91 @@ class MySingleList {
     //计算链表的长度
     public int getLength(){
         int count =0;
-        List_Node cur = this.head;
+        ListNode cur = this.head;
         while(cur != null){
             count++;
             cur = cur.next;
         }
         return count;
+    }
+
+    //查找是否包含关键字 key
+    public boolean contains1(int key){
+        ListNode cur = this.head;
+        while(cur !=null){
+            if(cur.data == key){
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+    public ListNode contains2(int key){
+        ListNode cur = this.head;
+        while(cur != null){
+            if(cur.data == key){
+                return cur;
+            }
+            cur =cur.next;
+        }
+        return null;
+    }
+
+    //用来找前驱
+    private ListNode searchPrev(int key){
+        ListNode prev = this.head;
+        while(prev.next != null){
+            if(prev.next.data == key){
+                return prev;
+            }
+            prev = prev.next;
+        }
+        return null;
+    }
+    //删除关键字 key 的节点
+    public void remove(int key){
+        //如果删除的节点是头节点
+        if(this.head.data == key){
+            this.head = this.head.next;
+            return ;
+        }
+        ListNode prev = searchPrev(key);
+        if(prev == null){
+            System.out.println("没有要删除的节点");
+            return ;
+        }
+        ListNode del = prev.next;
+
+        prev.next = del.next;
+    }
+
+    //删除所有值为 key  的节点
+    public void removeAllKey(int key){
+        ListNode prev = this.head;
+        ListNode cur = this.head.next;
+        while(cur != null){
+            if(prev.next.data == key){
+                prev.next = cur.next;
+                cur = cur.next;
+            }else {
+                prev =cur;
+                cur = cur.next;
+            }
+        }
+
+        //头要放在最后处理
+        if(this.head.data ==key ){
+            this.head = this.head.next;
+        }
+    }
+
+    //清空单链表
+    public void clear(){
+        while(this.head.next != null){
+            ListNode cur = this.head.next;
+            this.head.next = cur.next;
+        }
+        this.head = null;
     }
 
 
