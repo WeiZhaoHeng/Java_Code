a29cf9ff002d1d1f7eacb3cc563ec3eda1f6352a
WeiZhaoHeng <1332113981@qq.com>
2019-12-08 23:20:53 +0800

    nothing

--- 20191202_带随机指针的复制/src/MySingleList.java
+++ 20191202_带随机指针的复制/src/MySingleList.java
@@ -60,9 +60,9 @@ class MySingleList {
         ListNode cur = this.head;
         while(cur != null){
             if(cur.random == null){
-                cur.random = cur;
-                cur = cur.next;
+                cur.random = cur.next;
             }
+            cur = cur.next;
         }
     }
     //打印 cur 的地址
@@ -99,7 +99,34 @@ class MySingleList {
 
     //给定一个链表，每个节点包含一个额外增加的随机指针，该指针可以指向链表中的任何节点或空节点。
     //要求返回这个链表的深拷贝。
-    public  ListNode copyRandomLists(ListNode head){
+    public ListNode copyRandomLists(ListNode head){
+        if(head == null){
+            return null;
+        }
+        ListNode cur = head;
+        while(cur != null){
+            ListNode node = new ListNode(cur.data ,cur.next,null);
+            ListNode tmp = cur.next;
+            cur.next = node;
+            cur = tmp;
+        }
+        cur = head;
+        while(cur != null){
+            if(cur.random != null){
+                cur.next.random = cur.random.next;
+            }
+            cur = cur.next.next;
+        }
+        cur = head;
+        ListNode newHead = cur.next;
+        while(cur.next != null){
+            ListNode tmp = cur.next;
+            cur.next = tmp.next;
+            cur = tmp;
+        }
+        return newHead;
+    }
+/*    public  ListNode copyRandomLists(ListNode head){
         if(head == null){
             return null;
         }
@@ -130,7 +157,7 @@ class MySingleList {
             cur = tmp;
         }
         return newHead;
-    }
+    }*/
 
     //打印 cur 的地址
     public void display3(ListNode head){
